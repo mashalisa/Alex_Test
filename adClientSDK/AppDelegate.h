b20249b022ed9b5ae4adfd25fee98804b4fdac79
Maria Levitin <marialevitin@Marias-Mac-mini.local>
2014-10-27 12:01:30 +0200

    master_file_al

---
+++ adClientSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adClientSDK
+//
+//  Created by Maria Levitin on 7/17/14.
+//  Copyright (c) 2014 adClientSDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ adClientSDKTests/adClientSDKTests.m
@@ -0,0 +1,34 @@
+//
+//  adClientSDKTests.m
+//  adClientSDKTests
+//
+//  Created by Maria Levitin on 7/17/14.
+//  Copyright (c) 2014 adClientSDK. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface adClientSDKTests : XCTestCase
+
+@end
+
+@implementation adClientSDKTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
